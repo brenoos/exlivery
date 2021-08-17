f6832ab64822ce940f931ae887b8991fc6992fbd
brenoos <breno.oliver07@gmail.com>
2021-08-17 14:50:25 -0300

    fix(user): putting address in the struct

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,18 +1,19 @@
 defmodule Exlivery.Users.User do
-  @keys [:name, :email, :cpf, :age]
+  @keys [:name, :address, :email, :cpf, :age]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(name, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
+  def build(name, address, email, cpf, age) when age >= 18 and is_bitstring(cpf) do
     {:ok,
      %__MODULE__{
        name: name,
+       address: address,
        email: email,
        cpf: cpf,
        age: age
      }}
   end
 
-  def build(_name, _email, _cpf, _age), do: {:error, "Invalid Parameters"}
+  def build(_name, _address, _email, _cpf, _age), do: {:error, "Invalid Parameters"}
 end
